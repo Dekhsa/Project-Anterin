b917fd27ad6aa4b2249cd731afba800b4cf032d9
Dekhsa <dekhsafnan@gmail.com>
2025-12-02 22:58:07 +0700

    feat: Add FoodItem component with item count functionality and context integration

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,10 +5,29 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const contextValue = {
-        food_list
+    const [cartItems, setCartitems] = useState({});
+
+    const addToCart = (itemId) => {
+        if(!cartItems[itemId]){
+            setCartitems((prev)=>({...prev, [itemId]:1}))
+        }
+        else{
+            setCartitems((prev)=>({...prev, [itemId]:prev[itemId]+1}))
+        }
     }
-    
+
+    const removeFromCart = (itemId) => {
+        setCartitems((prev)=>({...prev, [itemId]:prev[itemId]-1}))
+    }
+
+    const contextValue = {
+        food_list,
+        setCartitems,
+        cartItems,
+        addToCart,
+        removeFromCart
+    }
+
     return(
         <StoreContext.Provider value={contextValue}>
             {props.children}
